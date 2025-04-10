ee9d9e08431bf1085029942da9230e28177bafa6
StanTheThird <108234536+StanTheThird@users.noreply.github.com>
2025-04-10 02:02:15 -0400

    Cest si long
    Desoler. J'essaie d'avancer du mieux que je peut. Mais ya bcp à faire.

--- app/src/main/java/com/example/tch057_03_tp2/Historique.kt
+++ app/src/main/java/com/example/tch057_03_tp2/Historique.kt
@@ -3,15 +3,14 @@ package com.example.tch057_03_tp2
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.Composable
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Historique : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_historique)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
